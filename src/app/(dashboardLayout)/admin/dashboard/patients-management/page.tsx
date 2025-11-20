a61a6908a8141350cb7e0a5bbf1a06bb4963f887
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-11-20 11:58:24 +0600

    patient management feature added for admin

--- src/app/(dashboardLayout)/admin/dashboard/patients-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/patients-management/page.tsx
@@ -1,7 +1,53 @@
-export default function PatientManagementPage() {
+
+import PatientsManagementHeader from "@/components/modules/admin/patients-management/PatientsManagementHeader";
+import PatientsTable from "@/components/modules/admin/patients-management/PatientsTable";
+import RefreshButton from "@/components/shared/RefreshButton";
+import SearchFilter from "@/components/shared/SearchFilter";
+import TablePagination from "@/components/shared/TablePagination";
+import { TableSkeleton } from "@/components/shared/TableSkeleton";
+import { queryStringFormatter } from "@/lib/formatters";
+import { getPatients } from "@/services/admin/patientManagement";
+
+import { Suspense } from "react";
+
+export default async function PatientManagementPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const searchParamsObj = await searchParams;
+  const patients = await getPatients(queryStringFormatter(searchParamsObj));
+ 
+  const totalPages = Math.ceil(patients?.meta?.total / patients?.meta?.limit);
+  const currentPage = patients?.meta?.page
+  
   return (
-    <div>
-      <h1>This is PatientManagementPage</h1>
-    </div>
+    <div className="space-y-5">
+          <PatientsManagementHeader></PatientsManagementHeader>
+          <div className="flex items-center gap-2">
+            <SearchFilter
+              paramName="searchTerm"
+              placeholder="Search patients..."
+            ></SearchFilter>
+            {/* <SelectFilter
+              paramName="Specialty"
+              options={specialtiesResult?.data?.map((specialty: ISpecialty) => ({
+                label: specialty.title,
+                value: specialty.id,
+              }))}
+              placeholder="Filter by Specialty"
+            ></SelectFilter> */}
+            <RefreshButton></RefreshButton>
+          </div>
+          <Suspense fallback={<TableSkeleton columns={10} rows={5} />}>
+            <PatientsTable
+              patients={patients?.data}
+            ></PatientsTable>
+          </Suspense>
+          <TablePagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+          ></TablePagination>
+        </div>
   );
 }
